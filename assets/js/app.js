ed41a6b72c9153148b6fcae57f5e04f0a0c9db1d
fairbrain <fairbrainpacksol@gmail.com>
2024-07-25 11:57:50 +0530

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,28 @@ $('.testimonials .owl-carousel').owlCarousel({
 })
 
 
+// CLients Slider
+
+$(document).ready(function () {
+  $(".client-slider  .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
+
 
 const mainHead = document.querySelector(".main-head");
 const scrollY = window.scrollY; // Initial scroll position
